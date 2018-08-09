1dc9e7baf73330c4b38c725e336993ef18354ed4
Jameson <jamesoncoletucker@yahoo.com>
2018-08-08 21:09:18 -0400

    Inital commit

---
+++ Source/TestingGrounds/TestingGroundsGameMode.h
@@ -0,0 +1,19 @@
+// Copyright 1998-2018 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "TestingGroundsGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ATestingGroundsGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ATestingGroundsGameMode();
+};
+
+
+
