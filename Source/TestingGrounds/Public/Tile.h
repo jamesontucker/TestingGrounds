7160aa7c3d42e45e6bc88f31f5296a7dc0da5c9b
Jameson <jamesoncoletucker@yahoo.com>
2018-09-04 16:56:11 -0400

    Added tile class and updated asset names

---
+++ Source/TestingGrounds/Public/Tile.h
@@ -0,0 +1,30 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Tile.generated.h"
+
+UCLASS()
+class TESTINGGROUNDS_API ATile : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ATile();
+
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	UFUNCTION(BlueprintCallable, Category = "Placement")
+	void PlaceActors();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	
+	
+};
